bcd533de4f524a0fbf7880720c2841e8d27633ff
John <ripewhensoft@gmail.com>
2015-11-22 15:12:20 -0600

    / ‘pp.rb’ + ‘static/MetalStormBold.ttf’ + ‘static/jquery-1.11.3.min.js’ + ‘static/storage/108___06.rar’ + ‘static/storage/12203980_1134263346603863_1047204279_o.jpg’ + ‘static/storage/1447264509868.jpg’ + ‘static/storage/BruteForcer_091.7z’ + ‘static/storage/john-1.7.9-jumbo-7.tar.bz2’ + ‘static/storage/notice.txt’ + ‘static/stylesheet.css’ ...

--- app.rb
+++ app.rb
@@ -3,36 +3,56 @@ require 'gdbm'
 
 # gdbm = GDBM.new("")
 
-users = {'admin' => 'pass', 'admin2' => 'pass2'}
-usrclaim = {} #container for what was entered in boxes
-saying = ['The violent plunger says "Heyoooo".', 'Betsy dont play like that.', 'That boy aint right.']
-randomsaying = saying.sample
+users = {'admin' => 'pass', 'admin2' => 'pass2'} #usr/pass combos
+usrclaim = {} #login try
+saying = ['The violent plunger says "Heyoooo".', 'Betsy dont play like that.', 'That boy aint right.'] #failure responses
+randomsaying = saying.sample #grab random failure response
+$usrid = "nil" #users username for the session
+
+
 
 set :port, 8080
 set :static, true
 set :public_folder, "static"
 set :views, "views"
 
-get '/' do
-    return 'Hello world'
-end
 
-get '/hello/' do
+get '/' do
     erb :hello_form
 end
 
-post '/hello/' do
-    usr = params[:usr] 
+post '/workspace' do
+    usr = params[:usr] #get usr "name" from hello_form.erb
     pass = params[:pass]
 
     usrclaim[usr] = pass
     usrcheck = users.select { |k,v| k == usr }
     puts usrcheck
     if usrcheck == usrclaim then
-    	puts "youdidit"
-    	'success'
+    	usrclaim = {}
+    	$usrid = usr
+    	erb :success, :locals => {'usrcheck' => usrcheck}
     else
+    	usrclaim = {}
     	erb :failed, :locals => {'randomsaying' => randomsaying}
     end
 
+end
+
+post '/save_image' do
+  
+  filename = params[:file][:filename]
+  file = params[:file][:tempfile]
+
+  File.open("./storage/#{filename}", 'wb') do |f|
+    f.write(file.read)
+  end
+  erb :success
+end
+
+post '/chat' do
+	msg = params[:usrinput]
+	input = "#{$usrid}: #{msg}\n"
+	File.open('./chat.txt', 'a') { |file| file.write(input) }
+	erb :success
 end
